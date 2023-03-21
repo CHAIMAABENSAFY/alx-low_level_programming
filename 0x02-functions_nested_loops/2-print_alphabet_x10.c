1309fffdff5d206ae5616fdad43d0aa408915848
chaimaa <chaimaabensafy@gmail.com>
2023-03-21 14:44:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 {
 	int L, ch;
 
-	for (L = 0; L<= 9; L++)
+	for (L = 0; L <= 9; L++)
 	{
-		for (ch = 'a'; ch<= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}
