c9a2529b8e13a19c95777168fa35041fadd07c5e
chaimaa <chaimaabensafy@gmail.com>
2023-03-21 15:18:47 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 # include "main.h"
+
 /**
- * _islower 
+ * _islower - function to check if
+ *    	      character is lowercase
  *
  * 0c: checks input of function
  *
  * Return: returns 1 if 'c' is lowercase
- * otherwise always 0 (success)
+ *         otherwise always 0 (success)
  */
 int _islower(int c)
 {
