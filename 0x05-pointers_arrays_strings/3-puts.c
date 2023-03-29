5b98447dfb81c7795569c5d648d04bce161808b0
chaimaa <chaimaabensafy@gmail.com>
2023-03-29 15:29:44 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
-# include "main.h"
+#include "main.h"
+
 /**
- * _puts - prints a string
- * @str: the string to print
- * Return: void
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ *
+ * Return: 0
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i = 0;
+	
+	while (str[i])
 	{
-	_putchar(*str);
+	_putchar(str[i]);
+	i++;
 	}
 	_putchar('\n');
 }
