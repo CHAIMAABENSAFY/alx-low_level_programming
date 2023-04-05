a694d85e7bacccfd14f9c680d01c02d17a51c5e4
chaimaa <chaimaabensafy@gmail.com>
2023-04-05 10:38:06 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 # include "main.h"
  /**
-  *  _puts_recursion - Print a string followed by new line
-  * @s: the string to print
-  *
-  * Return: nothing
+  *  _puts_recursion - function prints a string, followed by a new line
+  * @s: input
+  * Return: Always 0
   */
 void _puts_recursion(char *s)
 {
